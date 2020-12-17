a28dc6aba277ffae318e157c0b9d1a169542ad1f
Wotcher-cock <74008903+Wotcher-cock@users.noreply.github.com>
2020-12-18 04:53:16 +0500

    Version2.0
    Сделал код с MVC

--- Form1.cs
+++ Form1.cs
@@ -12,38 +12,81 @@ namespace OOP4._2
 {
     public partial class Form1 : Form
     {
+        Model model;
         public Form1()
         {
             InitializeComponent();
+            model = new Model();
+            model.observers += new System.EventHandler(this.UpdateFromModel);
         }
+
+        public class Model
+        {
+            private int value;
+            public int check;
+            public System.EventHandler observers;
+
+            public void setValue(int value, int check)
+            {
+                this.value = value;
+                this.check = check;
+                observers.Invoke(this, null);
+            }
+            public int getValue()
+            {
+                return value;
+            }
+
+            public int getCheck()
+            {
+                return check;
+            }
+
+        }
+
+        private void UpdateFromModel(object sender, EventArgs e)
+        {
+            TrB.Value = model.getValue();
+            PB.Value = model.getValue();
+            if (model.getCheck() == 0)
+            {
+                TB.Text = (model.getValue() + 1).ToString();
+            }
+            else if (model.getCheck() == 1)
+            {
+                TB.Text = model.getValue().ToString();
+                NUP.Text = (model.getValue() - 1).ToString();
+            }
+            else
+            {
+                NUP.Text = model.getValue().ToString();
+                TB.Text = (model.getValue() + 1).ToString();
+            }
+        }
+    
+
 
         private void EnterB_Click(object sender, EventArgs e)
         {
-            MessageBox.Show(NUP.Value.ToString());
+            MessageBox.Show(model.getValue().ToString());
         }
 
         private void NUP_ValueChanged(object sender, EventArgs e)
         {
-            PB.Value = Decimal.ToInt32(NUP.Value);
-            TB.Text = (NUP.Value + 1).ToString();
-            TrB.Value = Decimal.ToInt32(NUP.Value);
+            model.setValue(Decimal.ToInt32(NUP.Value), 0);
         }
 
         private void TB_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Enter)
             {
-                NUP.Value = Int32.Parse(TB.Text) - 1;
-                PB.Value = Int32.Parse(TB.Text);
-                TrB.Value = Int32.Parse(TB.Text);
+                model.setValue(Int32.Parse(TB.Text), 1);
             }
         }
 
         private void TrB_Scroll(object sender, EventArgs e)
         {
-            NUP.Value = Decimal.ToInt32(TrB.Value);
-            PB.Value = Decimal.ToInt32(TrB.Value);
-            TB.Text = (TrB.Value+1).ToString();
+            model.setValue(TrB.Value, 2);
         }
     }
 }
